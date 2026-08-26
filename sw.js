647ba089409cee0903ab84d955eda3cdff33ce93
hvmoderno <hvmoderno@hotmail.com>
2026-08-26 07:57:42 -0300

    Bump SW

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    abre offline, mas pega a versão nova assim que houver rede.
    ========================================================================== */
 
-const VERSAO = 'v116';
+const VERSAO = 'v117';
 const CACHE = 'medtrabalho-' + VERSAO;
 
 const ARQUIVOS = [
